1175c5c1256ad628afdc87a269ee3811a2fac289
GavArmstrong <gavin.k.armstrong@gmail.com>
2020-12-27 20:25:53 -0700

    A random line joining two of the points.

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,11 +1,10 @@
 var canvas = document.getElementById("myCanvas");
-var cntxt = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
-console.log(cntxt);
+console.log(ctx);
 
-// A simple ball and paddle game.
 
-// VARIABLES.
+// GLOBAL VARIABLES.
 // The ball starting position.
 var ballRadius = 10;
 
@@ -22,6 +21,10 @@ var y = centerY;
 var pointX = [];
 var pointY = [];
 
+var ballColor1 = "#619CFB";
+var ballColor2 = "#EC746B";
+var lineColor = "#60BB3B";
+
 // Get a random integer. Min is included, Max is not.
 function randomInt(min, max) {
   min = Math.ceil(min);
@@ -31,26 +34,56 @@ function randomInt(min, max) {
 
 // The function for drawing the balls.
 function drawBall() {
-  cntxt.beginPath();
-  cntxt.arc(x, y, ballRadius, 0, Math.PI*2);
-  cntxt.fillStyle = "#59C0C5";
-  cntxt.fill();
-  cntxt.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = ballColor1;
+  ctx.fill();
+  ctx.closePath();
+}
+
+// The function for drawing lines between randomly selected points.
+function drawRandomLine() {
+  var pointOne = randomInt(0, numCircles - 1);
+
+  if (numCircles > 1) {
+    var pointTwo = randomInt(pointOne + 1, numCircles);
+  } else {
+    var pointTwo = 1;
+  }
+
+  console.log(pointOne, pointTwo);
+
+  console.log(pointX[pointOne], pointY[pointOne]);
+  console.log(pointX[pointTwo], pointY[pointTwo]);
+
+  ctx.beginPath();
+  ctx.moveTo(pointX[pointOne], pointY[pointOne]);
+  ctx.lineTo(pointX[pointTwo], pointY[pointTwo]);
+  ctx.lineWidth = 8;
+  ctx.strokeStyle = lineColor;
+  ctx.stroke();
+  ctx.closePath();
 }
 
 // Rendering the objects.
 function draw() {
-  cntxt.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   for (var i = 0; i < numCircles; i++) {
     x = centerX + circleRadius * Math.cos(i * 2 * Math.PI / numCircles);
     y = centerY + circleRadius * Math.sin(i * 2 * Math.PI / numCircles);
     pointX.push(x);
     pointY.push(y);
+  }
+
+  drawRandomLine();
+
+  for (var i = 0; i < pointX.length; i++) {
+    x = pointX[i];
+    y = pointY[i];
     drawBall();
   }
 
-  console.log(pointX[randomInt(0,10)]);
 }
 
 draw();
